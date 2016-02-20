52dc6fe28dba0f709b6beb249334efb72bc24a72
Clinton Beasley <cbeasley92@gmail.com>
2016-02-20 15:41:43 +0000

    Setting up Flask server
    Adding Coordinates

--- Web/backend.py
+++ Web/backend.py
@@ -1,69 +1,53 @@
 import requests
+import os
+
 from firebase import firebase
+
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
 from uber_rides.auth import AuthorizationCodeGrant
-from flask import Flask
+from flask import Flask, render_template, request, json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
+app.requests_session = requests.Session()
+app.secret_key = os.urandom(24)
+
+# FireBaseAuth
+firebase = firebase.FirebaseApplication('https://uberconcierge.firebaseIO.com', None)
+
 
 @app.route("/hello")
 def hello():
     return "Hello World!"
     print 'Hi Kyle'
 
+'''
 @app.route('/hello')
 def api_hello():
     if 'name' in request.args:
         return 'Hello ' + request.args['name']
     else:
         return 'Hello Peeps'
-
-if __name__ == "__main__":
-    app.run()
-
-#UberAuth
-session = Session(server_token="xPw_C1UY7WlYZKO9B3-Y92hI77sJkHfq3q2pcvq6")
-
-@app.route("/authorize")
-#UberUserAuth
-def uberUserAuth(YOUR_CLIENT_ID,YOUR_PERMISSION_SCOPES,YOUR_CLIENT_SECRET, YOUR_REDIRECT_URL):
-    auth_flow = AuthorizationCodeGrant(
-        YOUR_CLIENT_ID,
-        YOUR_PERMISSION_SCOPES,
-        YOUR_CLIENT_SECRET,
-        YOUR_REDIRECT_URL,
-    )
-    auth_url = auth_flow.get_authorization_url()
-    session = auth_flow.get_session(redirect_url)
-    client = UberRidesClient(session)
-    credentials = session.oauth2credential #save credentials in firebase datastore later
-    result = firebase.post('/users', credentials, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'}) #take a look at this line later
-
-#FireBaseAuth
-firebase = firebase.FirebaseApplication('https://uberconcierge.firebaseIO.com', None)
-
-#getAvailableProductsFromLocation
-@app.route("/get_products")
-def getAvailableProducts():
-    client = UberRidesClient(session)
-    locationLat = getLocationLat()
-    locationLong = getLocationLong()
-    response = client.get_products(locationLat, locationLong) #put in lat/long from google maps api on frontend
-    products = response.json.get('products')
+'''
 
 
-def getCurrentLocationLat():
-    # Returns Lat Long Coordinates
-    # From Google Maps API
+@app.route("/coordinates", methods=['POST'])
+def post_coordinates():
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+
+    print 'Coordinates: (%s,%s)' % (latitude, longitude)
+
+    return json.dumps({'status': 'OK', 'latitude': latitude, 'longitude': longitude})
+
+
+@app.route("/coordinates", methods=['GET'])
+def get_coordinates():
     latitude = 0
-    return latitude
+    longitude = 0
+    return json.dumps({'status': 'OK', 'latitude': latitude, 'longitude': longitude})
 
-def getCurrentLocationLong():
-    longitude = 0;
-    return longitude
-    
-def callUberRide():
-    response = client.request_ride(product_id, 37.77, -122.41, 37.79, -122.41)
-    ride_details = response.json
-    ride_id = ride_details.get('request_id')
+
+if __name__ == '__main__':
+    app.debug = os.environ.get('FLASK_DEBUG', True)
+    app.run(port=7000)
