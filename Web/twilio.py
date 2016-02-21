caafa99d8cc3fd5daa65f1411111a21b51d4b0c8
chaitanyavaranasi <chaitanya.varanasi@utexas.edu>
2016-02-20 21:31:29 -0600

    add twilio auth/method

--- Web/twilio.py
+++ Web/twilio.py
@@ -3,10 +3,10 @@ from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://uberconcierge.firebaseIO.com', None)
 
-account = "ACXXXXXXXXXXXXXXXXX"
-token = "YYYYYYYYYYYYYYYYYY"
+account = "ACe0331ea5a35b43b297aef69a445be390"
+token = "bcded206e1255906567f482ad8cf5768"
 client = TwilioRestClient(account, token)
 
 def sendMessage():
-    message = client.messages.create(to="+12316851234", from_="+15555555555",
+   message = client.messages.create(to="+12818511567", from_="+17135975622",
                                  body="Hello there!")
