7f56d62a00ea85d137fa28d23d85e86d69e33382
kkiple93 <kkiple93@gmail.com>
2016-02-21 00:45:54 -0600

    v7

--- Web/test.js
+++ Web/test.js
@@ -1,4 +1,13 @@
-var text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+  console.log("Hello");
+  function uberLogin(){
+            var clientId = "HVXgAbDzXyvreW-pctRCHtrvXbYieGD5";
+            var redirect = "http://localhost:7000/submit";
+            var responseType = "code";
+            
+            $.ajax({
+                type: "GET",
+                url: "https://login.uber.com/oauth/v2/authorize?client_id=" + clientId + "&response_type=" + responseType
+            }).done(function(response){
+                console.log(response);
+            });
+        }
